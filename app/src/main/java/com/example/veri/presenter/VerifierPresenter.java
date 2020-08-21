9a18d8e1351189bbed81f8aaacb1a251704a6580
David Galleguillos <galleguillos.david@gmail.comglobal user.email galleguillos.david@gmail.com>
2020-08-20 23:01:29 -0400

    fin

--- app/src/main/java/com/example/veri/presenter/VerifierPresenter.java
+++ app/src/main/java/com/example/veri/presenter/VerifierPresenter.java
@@ -7,8 +7,27 @@ public class VerifierPresenter implements IPresenter {
    private Verifier verifier;
    private IViewPresenter view;
 
+    public VerifierPresenter(IViewPresenter view) {
+        this.view = view;
+        verifier= new Verifier();
+    }
+
     @Override
     public void evaluatePass(String password) {
+        int res=verifier.evaluatePass(password);
+        switch (res){
+            case Verifier.WEAK:
+                this.view.showWeak();
+                break;
+                
+                case Verifier.MEDIUM:
+                    this.view.showMedium();
+                    break;
+            case Verifier.STRONG:
+                this.view.showStrong();
 
+
+
+        }
     }
 }
