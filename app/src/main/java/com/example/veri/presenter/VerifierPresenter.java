ef569f2c3ba1e8d015ec416b1446e4745b9fca63
David Galleguillos <galleguillos.david@gmail.comglobal user.email galleguillos.david@gmail.com>
2020-08-20 22:48:48 -0400

    ya

--- app/src/main/java/com/example/veri/presenter/VerifierPresenter.java
+++ app/src/main/java/com/example/veri/presenter/VerifierPresenter.java
@@ -1,6 +1,12 @@
 package com.example.veri.presenter;
 
+import com.example.veri.model.Verifier;
+
 public class VerifierPresenter implements IPresenter {
+
+   private Verifier verifier;
+   private IViewPresenter view;
+
     @Override
     public void evaluatePass(String password) {
 
